d2dfddc7ad0942bc58b278576f9177dae3e02d81
Tanvi sharma <sharmatanvi0708@gmail.com>
2026-03-21 23:19:49 +0530

    Add files via upload
    added delete and mark complete feature

--- script.js
+++ script.js
@@ -7,12 +7,19 @@ function addTask() {
   let li = document.createElement("li");
   li.textContent = task;
 
+  // MARK COMPLETE (CLICK)
+  li.onclick = function () {
+    li.style.textDecoration = "line-through";
+    li.style.color = "gray";
+  };
+
   // DELETE BUTTON
   let deleteBtn = document.createElement("button");
   deleteBtn.textContent = "❌";
 
-  deleteBtn.onclick = function () {
-    li.remove();
+  deleteBtn.onclick = function (event) {
+  event.stopPropagation(); // IMPORTANT LINE
+  li.remove();
   };
 
   li.appendChild(deleteBtn);
